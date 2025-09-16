3544a61b9496a06efdf9d624cdfc51551b37ff42
SuDev <sh36912151821@gmail.com>
2025-09-17 00:35:52 +0900

    Update J0915.java

--- codes/J0915.java
+++ codes/J0915.java
@@ -8,14 +8,20 @@ public class J0915 {
 		String input; //input : 입력받은 문자열 저장할 변수
 		String ex = "잘못된 입력입니다.\n-----------------------------------\n"; //ex : 예외시 출력할 텍스트
 		while(true) {
-			System.out.print("성적을 입력하시오. (정수로만 입력하시오.) \n입력 중지시 q를 입력하시오.\n>> ");
+			System.out.print("성적을 입력하시오. (정수로만 입력하시오.) \n입력 중지시 q 혹은 Q 아니면 음수를 입력하시오.\n>> "); //성적 입력 조건 설명
 			input = sc.next(); //input에 성적 입력받기
 			try { //try catch 예외처리
-				if(input.charAt(0)=='q') {  //q를 입력받으면 무한반복 탈출 / 스트링변수.charAt(인덱스) : 스트링변수의 인덱스번째의 글자를 char 자료형으로 변환
-					break;
+				if(input.charAt(0)=='q' || input.charAt(0)=='Q') {  //q나 Q를 입력받으면 무한반복 탈출 / 스트링변수.charAt(인덱스) : 스트링변수의 인덱스번째의 글자를 char 자료형으로 변환
+					break; //질문 할꺼 : q나 quit 아니 qawhdwseir를 입력받아도 프로그램이 종료됨. -> q나 Q 하나만 들어가있는지 확인할 방법?
 				}
 				else { //숫자 입력
 					score = Integer.parseInt(input); //문자열로 입력받은 숫자를 Integer.parseInt(변수명)를 이용해 정수형으로 변환
+					if(score < 0) //입력받은 값이 음수라면 탈출
+						break;
+					else if(score > 100){ //입력받은 값이 100보다 넘는다면 밑 코드를 실행하지 않고 반복문 처음으로 돌아감.
+						System.out.print("점수는 0~100 까지의 숫자만 넣을 수 있습니다.");
+						continue;
+					}
 					sum+=score; //sum에 score값을 추가해서 저장
 					many++; //카운트 증가
 				}
@@ -27,7 +33,7 @@ public class J0915 {
 			System.out.print("입력된 점수가 없습니다."); //카운트 값이 0인데 무한루프 탈출시 예외 출력
 		}
 		else {
-			int avg=sum/many; //avg 평균 계산
+			double avg=(double)sum/many; //avg 평균 계산 sum을 (double)로 형변환
 			char gra; //gra 문자 변수 생성
 			if(avg>=90) //평균 등급 계산
 				gra = 'A'; 
